5a46e751dd197efca933191a9016fff16560f365
Rankshow <uwaomaobinna20@gmail.com>
2023-05-15 21:48:06 +0100

    Api creation

---
+++ data.js
@@ -0,0 +1,12 @@
+// generate random data
+module.exports = () => {
+  const data = {
+    products: []
+  }
+  for ( let i = 0; i < 1000; i++){
+    data.products.push({
+      id: i, title: `product${1}`
+    })
+  }
+  return data
+}
